d74b6c0b856e0d82800c39a1f6f6d6f2885eb99e
Inj3 <58576616+Inj3-GT@users.noreply.github.com>
2023-06-23 19:53:25 +0200

    v1.0

---
+++ improved_darkrp_script/lua/autorun/server/ipr_server.lua
@@ -0,0 +1,38 @@
+--- // Configuration
+local ipr_job_bl = { --- // Les métiers qui ne seront pas affectés et qui garderont leur model spéciaux défini dans le fichier job !
+    ["Hobo"] = true,
+}
+local ipr_mdl_c = { -- Les models randoms qui seront définis lors du changement de job sauf spécifer dans "ipr_job_bl".
+    "models/player/combine_soldier_prisonguard.mdl",
+    "models/player/group01/female_04.mdl",
+}
+---
+
+hook.Add("PlayerChangedTeam", "ipr_nochanged_model", function(ply, old, new)
+    if (ipr_job_bl[team.GetName(old)] or ipr_job_bl[team.GetName(new)]) then
+        return
+    end
+
+    timer.Simple(0.01, function()
+        if not IsValid(ply) then
+            return
+        end
+        local ipr_mdl = table.Random(ipr_mdl_c)
+
+        ply:SetModel(ipr_mdl)
+    end)
+end)
+
+hook.Add("PlayerInitialSpawn", "ipr_nochanged_init", function(ply)
+    timer.Simple(1, function()
+        if not IsValid(ply) then
+            return
+        end
+        if ipr_job_bl[team.GetName(ply:Team())] then
+            return
+        end
+        local ipr_mdl = table.Random(ipr_mdl_c)
+
+        ply:SetModel(ipr_mdl)
+    end)
+end)
