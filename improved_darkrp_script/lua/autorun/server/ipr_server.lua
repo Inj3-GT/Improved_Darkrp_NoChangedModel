57a5cc0513f089ddcf324eaaec969d6464f92476
Inj3 <58576616+Inj3-GT@users.noreply.github.com>
2023-06-23 21:03:17 +0200

    v1.1

--- improved_darkrp_script/lua/autorun/server/ipr_server.lua
+++ improved_darkrp_script/lua/autorun/server/ipr_server.lua
@@ -3,24 +3,25 @@ local ipr_job_bl = { --- // Les métiers qui ne seront pas affectés et qui gard
     ["Hobo"] = true,
     ["Pompier"] = true,
 }
-local ipr_mdl_c = { -- Les models randoms qui seront définis lors du changement de job pour tous les métiers sauf spécifer dans "ipr_job_bl".
+local ipr_mdl_c = { -- Les models randoms qui seront définis lors du changement de job // sauf spécifer dans "ipr_job_bl".
     "models/player/combine_soldier_prisonguard.mdl",
     "models/player/group01/female_04.mdl",
 }
+local ipr_keep = true -- Garder la même tenue entre les changements de métier ?
 ---
 
 hook.Add("PlayerChangedTeam", "ipr_nochanged_model", function(ply, old, new)
     if (ipr_job_bl[team.GetName(old)] or ipr_job_bl[team.GetName(new)]) then
         return
     end
+    local ipr_c = (ipr_keep) and ply:GetModel() or table.Random(ipr_mdl_c)
 
     timer.Simple(0.01, function()
         if not IsValid(ply) then
             return
         end
-        local ipr_mdl = table.Random(ipr_mdl_c)
 
-        ply:SetModel(ipr_mdl)
+        ply:SetModel(ipr_c)
     end)
 end)
 
@@ -32,8 +33,8 @@ hook.Add("PlayerInitialSpawn", "ipr_nochanged_init", function(ply)
         if ipr_job_bl[team.GetName(ply:Team())] then
             return
         end
-        local ipr_mdl = table.Random(ipr_mdl_c)
+        local ipr_m = table.Random(ipr_mdl_c)
 
-        ply:SetModel(ipr_mdl)
+        ply:SetModel(ipr_m)
     end)
 end)
