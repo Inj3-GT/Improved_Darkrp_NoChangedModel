30a6039b4389c70e6b6f2686a72e8b31516c22e2
Inj3 <58576616+Inj3-GT@users.noreply.github.com>
2023-06-25 10:52:49 +0200

    v1.3

--- improved_darkrp_script/lua/autorun/server/ipr_server.lua
+++ improved_darkrp_script/lua/autorun/server/ipr_server.lua
@@ -1,40 +1,69 @@
+----------- // SCRIPT BY INJ3 
+----------- // SCRIPT BY INJ3 
+----------- // SCRIPT BY INJ3 
+----------- // https://steamcommunity.com/id/Inj3/
+----------- https://github.com/Inj3-GT
+local ipr_config = {}
+
 --- // Configuration
-local ipr_job_bl = { --- // Les métiers qui ne seront pas affectés et qui garderont leur model spéciaux défini dans le fichier job du darkrp !
+ipr_config.jb = { --- // Les métiers qui ne seront pas affectés et qui garderont leur model spéciaux défini dans le fichier job du darkrp !
     ["Hobo"] = true,
     ["Pompier"] = true,
 }
-local ipr_mdl_c = { -- Les models randoms qui seront définis lors du changement de job // sauf spécifer dans "ipr_job_bl".
+ipr_config.md = { -- Les models randoms qui seront définis lors du changement de job // sauf spécifer dans "ipr_job_bl".
     "models/player/combine_soldier_prisonguard.mdl",
     "models/player/group01/female_04.mdl",
 }
-local ipr_keep = true -- Garder la même tenue entre les changements de métier ?
+ipr_config.kp = true -- Garder la même tenue entre les changements de métier ?
 ---
 
-hook.Add("PlayerChangedTeam", "ipr_nochanged_model", function(ply, old, new)
-    if (ipr_job_bl[team.GetName(old)] or ipr_job_bl[team.GetName(new)]) then
-        return
-    end
-    local ipr_c = (ipr_keep) and ply:GetModel() or table.Random(ipr_mdl_c)
-
-    timer.Simple(0.01, function()
-        if not IsValid(ply) then
-            return
+---- Do not touch the code below
+-- //
+do
+    local function ipr_m(p, o)
+        if (ipr_config.kp) then
+            if (ipr_config.jb[team.GetName(o)]) then
+                return p.ipr_dkmdl
+            end
+            p.ipr_dkmdl = p:GetModel()
+            return p.ipr_dkmdl
+        else
+            return table.Random(ipr_config.md)
         end
 
-        ply:SetModel(ipr_c)
+        return p:GetModel()
+    end
+
+    local function ipr_d(o, n)
+        return ((o == 1001) or (n == 1001)) and true or false
+    end
+
+    hook.Add("PlayerChangedTeam", "ipr_nochanged_model", function(ply, old, new)
+        if (ipr_config.jb[team.GetName(new)]) or ipr_d(old, new) then
+            return
+        end
+        local ipr_c = ipr_m(ply, old)
+
+        timer.Simple(0.01, function()
+            if not IsValid(ply) then
+                return
+            end
+
+            ply:SetModel(ipr_c)
+        end)
     end)
-end)
+end
 
 hook.Add("PlayerInitialSpawn", "ipr_nochanged_init", function(ply)
     timer.Simple(1, function()
         if not IsValid(ply) then
             return
         end
-        if ipr_job_bl[team.GetName(ply:Team())] then
+        if ipr_config.jb[team.GetName(ply:Team())] then
             return
         end
-        local ipr_m = table.Random(ipr_mdl_c)
+        local ipr_r = table.Random(ipr_config.md)
 
-        ply:SetModel(ipr_m)
+        ply:SetModel(ipr_r)
     end)
 end)
