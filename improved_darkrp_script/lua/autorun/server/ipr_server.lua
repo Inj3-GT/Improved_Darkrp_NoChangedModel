52c6ed149c0cc53b2f23806d157c98a750499f28
Inj3 <58576616+Inj3-GT@users.noreply.github.com>
2023-07-06 16:27:22 +0200

    v1.1

--- improved_darkrp_script/lua/autorun/server/ipr_server.lua
+++ improved_darkrp_script/lua/autorun/server/ipr_server.lua
@@ -7,12 +7,15 @@ local ipr_config = {}
 
 --- // Configuration
 ipr_config.jb = { --- // Les métiers qui ne seront pas affectés et qui garderont leurs models spéciaux définis dans le fichier job du darkrp ! - Jobs that will not be affected and that will keep their special models defined in the darkrp job file!
-    ["Hobo"] = true,
-    ["Pompier"] = true,
+    ["Mayor"] = true,
+    ["Cook"] = true,
 }
-ipr_config.md = { -- Les models randoms qui seront définis lors du changement de job // sauf spécifer dans "ipr_config.jb". - The randoms model that will be defined when changing jobs // unless specified in "ipr_config.jb".
-    "models/player/combine_soldier_prisonguard.mdl",
+ipr_config.md = { -- Le model aléatoire qui sera défini à la première apparition du joueur ou lors du changement de métier // sauf spécifer dans "ipr_config.jb". - The randoms model that will be defined when changing jobs // unless specified in "ipr_config.jb".
+    "models/player/eli.mdl",
     "models/player/group01/female_04.mdl",
+    "models/player/kleiner.mdl",
+    "models/player/barney.mdl",
+    "models/player/p2_chell.mdl",
 }
 ipr_config.kp = true -- Garder la même tenue entre les changements de métier ? - Keeping the same outfit between job changes?
 --- //
@@ -20,7 +23,7 @@ ipr_config.kp = true -- Garder la même tenue entre les changements de métier ?
 ---- Do not touch the code below
 -- //
 do
-    local function ipr_m(p, o)
+    local function Ipr_RendMdl(p, o)
         if (ipr_config.kp) then
             if (ipr_config.jb[team.GetName(o)] and p.ipr_dkmdl) then
                 return p.ipr_dkmdl
@@ -33,15 +36,15 @@ do
         end
     end
 
-    local function ipr_d(o, n)
+    local function Ipr_CpTeam(o, n)
         return ((o == 1001) or (n == 1001)) and true or false
     end
 
-    hook.Add("PlayerChangedTeam", "ipr_nochanged_model", function(ply, old, new)
-        if (ipr_config.jb[team.GetName(new)]) or ipr_d(old, new) then
+    hook.Add("PlayerChangedTeam", "Ipr_NCM:DarkRP_ChangedT", function(ply, old, new)
+        if (ipr_config.jb[team.GetName(new)]) or Ipr_CpTeam(old, new) then
             return
         end
-        local ipr_c = ipr_m(ply, old)
+        local ipr_c = Ipr_RendMdl(ply, old)
 
         timer.Simple(0.01, function()
             if not IsValid(ply) then
@@ -52,8 +55,8 @@ do
         end)
     end)
     
-    hook.Add("PlayerSpawn", "ipr_nochanged_death", function(ply)
-        local ipr_c = ipr_m(ply, ply:Team())
+    hook.Add("PlayerSpawn", "Ipr_NCM:DarkRP_Death", function(ply)
+        local ipr_c = Ipr_RendMdl(ply, ply:Team())
 
         timer.Simple(0.01, function()
             if not IsValid(ply) then
@@ -65,7 +68,7 @@ do
     end)
 end
 
-hook.Add("PlayerInitialSpawn", "ipr_nochanged_init", function(ply)
+hook.Add("PlayerInitialSpawn", "Ipr_NCM:DarkRP_Init", function(ply)
     timer.Simple(1, function()
         if not IsValid(ply) then
             return
